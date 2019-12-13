041bd55e9f7eee38d5f018b7fc25b1597de4531e
EstradaDiego99 <devdiegoest@gmail.com>
2019-12-13 00:44:30 -0600

    Recuperar Home

--- js/home.js
+++ js/home.js
@@ -1,44 +1,37 @@
-var portraitHeight = $(".portrait-container").data("height") || $(".landing-video").height();
-checkPortraitHeight();
-checkHeaderColor();
+var portraitHeight =
+  $("header.top-menu").data("height") || $(".landing-video").height();
 
 $(".portrait-container").height(Math.min(window.screen.width, portraitHeight));
 $("video").height(Math.min(window.screen.width, portraitHeight));
-var scrollHeights = createScrollHeights();
 
 $(window).scroll(() => checkHomeElement());
 
-function createScrollHeights() {
-    let scrollHeights = [];
-    let first = parseInt($(".portrait-container").height() || 0) - window.innerHeight;
-    first += window.innerHeight / 2;
-    first += parseInt($(".portrait-container").css("margin-bottom") || 0);
-    scrollHeights.push(first);
-
-    let marginBot = 0;
-    $(".home-element").each(function (index) {
-        if (index == 0) return;
-        marginBot = parseInt($(this).css("margin-bottom") || 0);
-        scrollHeights.push((scrollHeights[index - 1] || 0) + $(this).outerHeight() + marginBot);
-    });
-
-    return scrollHeights;
-}
-
 function checkHomeElement() {
-    const h = $(document).scrollTop();
-    $(".home-element").each(function (index) {
-        if (h >= scrollHeights[index - 1] && h < (scrollHeights[index] || 999999)) {
-            $(this).stop(false).fadeTo("fast", 1);
-        } else {
-            $(this).stop(false).fadeTo("fast", 0.1);
-            if (index == 0 && h < scrollHeights[0]) $(this).stop(false).fadeTo("fast", 1);
-            if (index == scrollHeights.length - 1 && h > scrollHeights[scrollHeights.length - 1]) $(this).stop(false).fadeTo("fast", 1);
-        }
-    });
+  let foundCurrent = false;
+  const h = $(document).scrollTop();
+  const halfScreen = window.innerHeight / 2;
+  $(
+    $(".home-element")
+      .get()
+      .reverse()
+  ).each(function() {
+    if (h > $(this).offset().top - halfScreen) {
+      foundCurrent
+        ? $(this)
+            .stop(false)
+            .fadeTo("fast", 0.1)
+        : $(this)
+            .stop(false)
+            .fadeTo("fast", 1);
+      foundCurrent = true;
+    } else {
+      $(this)
+        .stop(false)
+        .fadeTo("fast", 0.1);
+    }
+  });
 }
 
-document.querySelector('video').addEventListener('ended', () => $('video').prop('muted', true));
-
-/* MAINTAIN PORTRAIG IMAGE SET */
-function checkPortraitHeight() {}
+document
+  .querySelector("video")
+  .addEventListener("ended", () => $("video").prop("muted", true));
